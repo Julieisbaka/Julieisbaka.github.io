b990c23b22ffad7e7e73473ce5d3263ff90c522e
Julieisbaka <casperschorr06@gmail.com>
2024-12-13 21:03:35 -0500

    test

--- New folder/bail.js
+++ New folder/bail.js
@@ -1,9 +1,9 @@
 //window.location = "http://loc.playspent.org/flash/";
-$(function() {
-     /**
-     * detect IE
-     * returns version of IE or false, if browser is not Internet Explorer
-     */
+$(function () {
+    /**
+    * detect IE
+    * returns version of IE or false, if browser is not Internet Explorer
+    */
     function detectIE() {
         var ua = window.navigator.userAgent;
 
@@ -22,17 +22,17 @@ $(function() {
 
         var edge = ua.indexOf('Edge/');
         if (edge > 0) {
-           // IE 12 => return version number
-           return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+            // IE 12 => return version number
+            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
         }
 
         // other browser
         return false;
     }
-    
+
     var IEversion = detectIE();
     //console.log(IEversion);
-    
+
     //add reject overlay for IE 8 & 9
     if (IEversion != false) {
         if (IEversion <= 9) {
@@ -40,16 +40,16 @@ $(function() {
             var chrome = '<a href="https://support.google.com/chrome/answer/95346?hl=en" target="_blank"><img src="' + imagePath + 'browser_chrome.gif"><Br/>Google Chrome</a>';
             var ie = '<a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie" target="_blank"><img src="' + imagePath + 'browser_msie.gif" ><Br/>Internet Explorer</a>';
             var firefox = '<a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank"><img src="' + imagePath + 'browser_firefox.gif" ><br/>Firefox</a>';
-            $('body').prepend('<div id="jr_wrap"><div id="jr_inner"><h1 id="jr_header">Did you know that your Internet Browser is out of date?</h1><p>Your browser is out of date, and may not be compatible with our website. A list of the most popular web browsers can be found below.</p><p>Just click on the icons to get to the download page</p><ul><li id="jr_chrome">'+ chrome +'</li><li id="jr_firefox">' + firefox + '</li><li id="jr_msie">' + ie + '</li></ul><p>By closing this window you acknowledge that your experience on this website maybe degraded.</p></div><div id="jr_close">Close This Window</div></div><div id="jr_overlay"></div>');
+            $('body').prepend('<div id="jr_wrap"><div id="jr_inner"><h1 id="jr_header">Did you know that your Internet Browser is out of date?</h1><p>Your browser is out of date, and may not be compatible with our website. A list of the most popular web browsers can be found below.</p><p>Just click on the icons to get to the download page</p><ul><li id="jr_chrome">' + chrome + '</li><li id="jr_firefox">' + firefox + '</li><li id="jr_msie">' + ie + '</li></ul><p>By closing this window you acknowledge that your experience on this website maybe degraded.</p></div><div id="jr_close">Close This Window</div></div><div id="jr_overlay"></div>');
             //close overlay
-            $('#jr_close').click(function() {
+            $('#jr_close').click(function () {
                 $('#jr_wrap').remove();
                 $('#jr_overlay').remove();
             });
         }
     }
-    
-    $("#donate-screen").find('.donate').click(function(e) {
+
+    $("#donate-screen").find('.donate').click(function (e) {
         Tracking._event(Tracking.category, "Give $5", "Click")
     })
 });
--- New folder/patch.js
+++ New folder/patch.js
@@ -1,13 +1,13 @@
-$(function() {
+$(function () {
     var socialMedia = false;
-    window.setTimeout(function() {
+    window.setTimeout(function () {
         socialMedia = {
-            FB : (typeof FB === "undefined") ? false : true,
-            TW : (typeof __twttr === "undefined") ? false : true
+            FB: (typeof FB === "undefined") ? false : true,
+            TW: (typeof __twttr === "undefined") ? false : true
         }
     }, 3000);
 
-    $(document).ajaxComplete(function(event, request, settings) {
+    $(document).ajaxComplete(function (event, request, settings) {
         //filter the social media actions based on the socialMedia object
         socialMedia && filterSocialMedia(socialMedia);
     });
@@ -15,21 +15,21 @@ $(function() {
     //filter the social media actions based on the socialMedia object
     function filterSocialMedia(socialMedia) {
         // entire social option container is linked to facebook, So have to disable click event on entire container first.
-        $('.option.social').css({'pointer-events':'none'});
-        
+        $('.option.social').css({ 'pointer-events': 'none' });
+
         // Hover state for social icons since the answer option title doesn't change color anymore
-        $('.share-facebook','.share-twitter').css('transition','all .2s');
-        $('.option.social').find('a').mouseover(function(){
-          $(this).find('.share-twitter').css('transform','scale(1.1)');
+        $('.share-facebook', '.share-twitter').css('transition', 'all .2s');
+        $('.option.social').find('a').mouseover(function () {
+            $(this).find('.share-twitter').css('transform', 'scale(1.1)');
         });
-        $('.option.social').find('a').mouseout(function(){
-          $(this).find('.share-twitter').css('transform','scale(1)');
+        $('.option.social').find('a').mouseout(function () {
+            $(this).find('.share-twitter').css('transform', 'scale(1)');
         });
-        $('.option.social').find('.share-facebook').mouseover(function(){
-          $(this).css('transform','scale(1.1)');
+        $('.option.social').find('.share-facebook').mouseover(function () {
+            $(this).css('transform', 'scale(1.1)');
         });
-        $('.option.social').find('.share-facebook').mouseout(function(){
-          $(this).css('transform','scale(1)');
+        $('.option.social').find('.share-facebook').mouseout(function () {
+            $(this).css('transform', 'scale(1)');
         });
 
         if (!socialMedia.FB && !socialMedia.TW) {
@@ -40,16 +40,16 @@ $(function() {
             // FB disabled - hide facebook, enable pointer events on Twitter button
             $('.share-facebook').hide();
             $('#facebook_share_icon').hide();
-            $('.option.social').find('a').css({'pointer-events':'auto'});
+            $('.option.social').find('a').css({ 'pointer-events': 'auto' });
         } else if (!socialMedia.TW) {
             // TW disabled - hide twitter, enable pointer events on facebook button
             $('.share-twitter').hide();
             $('#twitter_share_icon').hide();
-            $('.option.social').find('.share-facebook').css({'pointer-events':'auto'});
+            $('.option.social').find('.share-facebook').css({ 'pointer-events': 'auto' });
         } else {
             // Neither disabled - enable pointer events on both twitter and facebook buttons
-            $('.option.social').find('.share-facebook').css({'pointer-events':'auto'});
-            $('.option.social').find('a').css({'pointer-events':'auto'});
+            $('.option.social').find('.share-facebook').css({ 'pointer-events': 'auto' });
+            $('.option.social').find('a').css({ 'pointer-events': 'auto' });
         }
 
         return true;
